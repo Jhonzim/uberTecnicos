d4b67bee8c0bb4cda46035f11f39cea009fb3372
Jhonzim <joaopedroalmeida2004@gmail.com>
2022-09-08 23:14:55 -0300

    1º dia - commit 1

--- index.php
+++ index.php
@@ -5,34 +5,51 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta name="description" content="TCC 2022 - Nome sugestivo">
+    <meta name="keywords" content="Técnicos Informática Terceirização Uberização Conserto Manutenção Montagem ">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+
+    <title>Tec Share</title>
 </head>
-<body>
-    <section >
+<body >
+    <section>
         <div class="header">
             <div class="titulo_geral">
-                <a href="index.php"><img src="https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg" alt="Nome do site" style="width: 40px; height: 40px;"></a>
-                <h1>TCC - titulo do site</h1>
+                <a href="index.php"><img src="https://i.pinimg.com/736x/36/7b/32/367b3237ec96b332356c7d84c2221e8d.jpg" alt="Nome do site" style="width: 20px; height: 40px;"></a>
+                <h1>TEC Share</h1>
             </div>
-            <div >
-                <a href="#">Link 1 </a>
-                <a href="#">Link 2 </a>
-                <a href="#">Link 3 </a>
+            <div>
+                <a href="./pag2.php">Página 2</a>
+                <a href="./login.php">Login</a>
+                <a href="./cadastro.php">Cadastro</a>
             </div>
         </div>    
     </section>
     <section class="main">
         <div>
-            <h2>Slogan sei lá</h2><br>
-            "juntem-se a sociedade pq somos superiores" - APOLINÀRIO, 2022
+            <h2>Venha ser mais</h2><br>
+            <!--"Juntem-se a sociedade pq somos superiores" - APOLINÁRIO, 2022 -->
         </div>
     </section>
     <section class="footer">
         <div>
             <h2>SAC 0800 666 7090</h2>
+        </div>
+    </section>
+    <section class="header">
+        <div>
+        <div class="flex-container">
+  <header class="header">Header</header>
+  <aside class="aside aside1">Aside 1</aside>
+  <article class="main">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed ex turpis. Cras luctus nibh lectus, in ullamcorper ex tempor eleifend. Nulla bibendum, eros a consequat vestibulum, orci massa fermentum quam, sed commodo nunc ex vitae nisl. Aliquam ullamcorper interdum est nec tincidunt.</p>
+  </article>
+  <aside class="aside aside2">Aside 2</aside>
+  <footer class="footer">Footer</footer>
+</div>
         </div>
     </section>
 </body>
--- pag2.php
+++ pag2.php
@@ -1,12 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta name="description" content="TCC 2022 - Nome sugestivo">
+    <meta name="keywords" content="Técnicos Informática Terceirização Uberização Conserto Manutenção Montagem ">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="bootstrap/bootstrap.min.css">
+
+    <title>Tec Share Página 2</title>
 </head>
 <body>
-    <h1>Página 2</h1>   
+    <h1>Página 2</h1>
+    <a href="./index.php">Home</a>
 </body>
 </html>
